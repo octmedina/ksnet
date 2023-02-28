8ae038e06f7186a07125dfb35618dcd9a71c6efa
Romen Adan <romenadanksnet@gmail.com>
2023-02-28 16:12:30 +0000

    se corrige la funcion check_stata_forma a raiz del bug encontrado por Marc

--- R/check_stata_format.r
+++ R/check_stata_format.r
@@ -8,9 +8,10 @@
 #' is not necessary to previously select the columns, since the function detects which
 #' variables need to be modified.
 #'
+#' The class of the new vector is guessed.
+#'
 #' @param obj A dataframe or a vector
-#' @param return_labels Logical. Return variable values as integers or with its labels. Default TRUE (labels).
-#' @param new_class When `return_lavels` set to FALSE, The desired class of the vector/variables. Choices: 'numeric', 'character' or 'logical'. By default it is converted to numeric.
+#'
 #' @return The same object with its changed class.
 #' @export
 #'
@@ -31,99 +32,194 @@
 #' ecv2020 <- ecv2020 %>%
 #'    mutate( situacion_hogar = check_stata_format( situacion_hogar )  )
 
-check_stata_format <- function(obj, return_labels = TRUE){
+#' @rdname check_stata_format
+check_stata_format <- function( obj){
 
-    if( !is.data.frame(obj) ){
+    UseMethod('check_stata_format')
+}
 
-        attr_labels <- attr(obj,'labels') # extraemos los valores del vector haven_labelled
-        x_labels <- names(attr_labels) # y sus etiquetas
-        names(x_labels) <- attr_labels # creamos el named vector
+#' @rdname check_stata_format
+#' @export
+check_stata_format.data.frame <- function(obj){
 
-        # si tiene etiquetas (está pensado para el loop):
-        if( !is.null(attr(obj,'labels')) & return_labels  ){
-            obj <- x_labels[ as.character(obj) ]
-            obj <- factor(obj,x_labels)
-            names(obj) <- NULL
-        }
-        attr(obj,'format.stata') <- NULL # eliminamos este atributo
-        # attr(obj,'label') <- NULL # dejamos finalmente el de la pregunta
-        attr(obj,'labels') <- NULL # eliminamos las etiquetas de los factores
+    obj <- purrr::map_dfc( obj, function(x){
+        # limpiar los attributos extraños importados de spss y stata
+        x <- clean_attributes(x)
 
-        return(obj)
+        if( class(x)[1] == "haven_labelled" ){
 
-    } else if( is.data.frame(obj) ){
+            x <- check_stata_format( obj = x )
 
-        for (i in seq_len(ncol(obj)) ) {
-
-            var_temp <- obj[[i]]
-            # si no es un haven_labelled al menos le quitamos la etiqueta de format.stata
-            if( class(var_temp)[1] != "haven_labelled" ){
-                attr(var_temp,'format.stata') <- NULL
-                obj[[i]] <- var_temp
-
-            } else{
-                obj[[i]] <- check_stata_format(obj = var_temp, return_labels = return_labels )
-
-            }
         }
 
-        return( obj )
+        return(x)
+
+    }  )
+
+    return(obj)
+
+
+}
+
+#' @rdname check_stata_format
+#' @export
+check_stata_format.default <- function(obj){
+
+    # extraemos el vector de valores nombrado con labels y checkeamos
+    # si hay labels duplicados, el output es una lista con 2 elementos,
+    # los valueLabels y el segundo indicando si hay duplicados o no (NULL)
+    valueLabels_dup <- check_duplicate_label(obj)
+
+    valueLabels <- valueLabels_dup$valueLabels
+
+    # si hay duplicados, los sacamos del vector valueLabels y generamos
+    # el nuevo vector de forma diferenciada
+    if( length(valueLabels_dup) == 2 ){
+
+        valueLabels <- valueLabels[ !names(valueLabels) %in% names( valueLabels_dup$dupLabels ) ]
+
+        new_values <- names(valueLabels)
+        names(new_values) <- valueLabels
+
+        ## añadimos el duplicado pero diferenciando las etiquetas
+
+        new_valuesDup <- paste0( names(valueLabels_dup$dupLabels), valueLabels_dup$dupLabels )
+        names( new_valuesDup ) <- valueLabels_dup$dupLabels
+
+        ## unimos
+        new_values <- c(new_values, new_valuesDup)
+
+    } else {
+
+        new_values <- names(valueLabels)
+        names(new_values) <- valueLabels
 
     }
+
+    # class(obj) <- 'character'
+    attr(obj,'format.stata') <- NULL # eliminamos este atributo
+    attr(obj,'display_width') <- NULL # eliminamos este atributo
+    attr(obj,'pregunta') <- attr(obj,'label') # dejamos la pregunta
+    attr(obj,'label') <- NULL
+    attr(obj,'labels') <- NULL # eliminamos las etiquetas de los factores
+
+    obj <- new_values[ as.character(obj) ]
+
+    names(obj) <- NULL
+    names(new_values) <- NULL
+
+    # obj <- factor(obj, levels = new_values)
+    obj <- guessAndset_class( obj, new_values )
+
+    return( obj )
+
+}
+
+clean_attributes <- function(x){
+
+    attr(x,'format.stata') <- NULL
+    attr(x,'format.spss') <- NULL
+    attr(x,'display_width') <- NULL # eliminamos este atributo
+    attr(x,'pregunta') <- attr(x,'label')
+    attr(x,'label') <- NULL
+
+    return(x)
+}
+
+
+
+check_duplicate_label <- function(x){
+
+    valueLabels <- attr(x,'labels')
+
+    countLabels <- table( names( valueLabels ) )
+
+    dupLabels <- names( countLabels[ countLabels > 1 ] )# etiquetas asociadas a más de 1 value
+
+    out <- list( 'valueLabels' = valueLabels )
+
+    if( length(dupLabels) == 0 ){
+
+        out$dupLabels <- NULL
+
+    } else{
+        dup_valueLabels <- valueLabels[ grepl( dupLabels, names(valueLabels ) ) ]
+
+        out$dupLabels <- dup_valueLabels
+    }
+    return(out)
+}
+
+
+#' Adivina y modifica la clase de un vector
+#'
+#' Internamente recurre a \code{vroom::guess_type}.
+#'
+#' @param x Un vector.
+#'
+#' @param setlevels Los niveles de un posible factor. Por defecto NULL.
+#'
+#' @return Un vector con la clase 'adivinada'.
+#'
+#' @export
+guessAndset_class <- function(x, setlevels = NULL){
+
+    x <- as.character(x)
+
+    guessClass <- attr( vroom::guess_type( x ), 'class' )[1]
+
+    out <- switch (guessClass,
+                   collector_logical = as.logical(x),
+                   collector_double = as.numeric(x),
+                   collector_character = factor(x)
+    )
+
+    if(is.factor(out) & !is.null(setlevels) ){ levels(out) <- setlevels }
+
+    return(out)
 
 }
 
 
-# FUNCION ANTERIOR SIGUIENDO LA LOGICA DE METODOS:
-#' modify_haven_labelled <- function(obj,to = c('numeric','character','logical'),
-#'                                   return_labels = TRUE){
-#'     UseMethod('modify_haven_labelled')
-#' }
-#'
-#'
-#' #'  rdname modify_haven_labelled
-#' #'  export
-#' modify_haven_labelled.default <- function(obj, to = c('numeric','character','logical'),
-#'                                           return_labels){
-#'     convert_class(obj, to, returnLabels = return_labels)
-#' }
-#'
-#' #' rdname modify_haven_labelled
-#' #' export
-#' modify_haven_labelled.data.frame <- function(obj, to = c('numeric','character','logical'),
-#'                                              return_labels ){
-#'     to <- match.arg(to, c('numeric','character','logical') )
-#'
-#'     for (i in seq_along(obj)) {
-#'         if( class(obj[[i]])[1] != "haven_labelled" ){
-#'             next
-#'         } else{
-#'             obj[[i]] <- convert_class(obj[[i]], to, returnLabels = return_labels)
-#'         }
-#'
-#'     }
-#'     return(obj)
-#' }
-#'
-#' #'
-#' convert_class <- function(x, new_class = c('numeric','character','logical'),
-#'                           returnLabels ){
-#'
-#'     new_class <- match.arg(new_class, c('numeric','character','logical') )
-#'
-#'
-#'     attr_labels <- attr(x,'labels')
-#'     x_labels <- names(attr_labels)
-#'     names(x_labels) <- attr_labels
-#'
-#'     attr(x,'format.stata') <- NULL
-#'     attr(x,'label') <- NULL
-#'     attr(x,'labels') <- NULL
-#'     class(x) <- new_class
-#'
-#'     if( returnLabels ){
-#'         x <- x_labels[ x ]
-#'         names(x) <- NULL
-#'     }
-#'     return(x)
-#' }
+# esta funcion devolvia un error cuando habia labels repetidas
+# check_stata_format <- function(obj, return_labels = TRUE){
+#
+#     if( !is.data.frame(obj) ){
+#
+#         attr_labels <- attr(obj,'labels') # extraemos los valores del vector haven_labelled
+#         x_labels <- names(attr_labels) # y sus etiquetas
+#         names(x_labels) <- attr_labels # creamos el named vector
+#
+#         # si tiene etiquetas (está pensado para el loop):
+#         if( !is.null(attr(obj,'labels')) & return_labels  ){
+#             obj <- x_labels[ as.character(obj) ]
+#             obj <- factor(obj,x_labels)
+#             names(obj) <- NULL
+#         }
+#         attr(obj,'format.stata') <- NULL # eliminamos este atributo
+#         # attr(obj,'label') <- NULL # dejamos finalmente el de la pregunta
+#         attr(obj,'labels') <- NULL # eliminamos las etiquetas de los factores
+#
+#         return(obj)
+#
+#     } else if( is.data.frame(obj) ){
+#
+#         for (i in seq_len(ncol(obj)) ) {
+#
+#             var_temp <- obj[[i]]
+#             # si no es un haven_labelled al menos le quitamos la etiqueta de format.stata
+#             if( class(var_temp)[1] != "haven_labelled" ){
+#                 attr(var_temp,'format.stata') <- NULL
+#                 obj[[i]] <- var_temp
+#
+#             } else{
+#                 obj[[i]] <- check_stata_format(obj = var_temp, return_labels = return_labels )
+#
+#             }
+#         }
+#
+#         return( obj )
+#
+#     }
+#
+# }
